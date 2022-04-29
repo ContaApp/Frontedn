94a422ad18c7ab87bb4410a0b85beeb41f6d97a2
Aldahir Mayorga Garcia <samg.25sep@gmail.com>
2022-04-29 11:43:14 -0500

    Se agrega page ISR Retenido

--- pages/isrretenido.js
+++ pages/isrretenido.js
@@ -6,16 +6,16 @@ import LayoutPage from '../components/utilities/layout-page/LayoutPages';
 
 import StepperISR from '../components/utilities/Steppers/StepperISR';
 import InputMoney from '../components/utilities/Input/Input-money-pages';
-import LottieISRDeducible from '../components/Lotties/Lottie-isr-deducible';
+import LottieISRRetenido from '../components/Lotties/Lottie-isr-retenido';
 
 import next from '../public/assets/icons/Next.svg';
 import prev from '../public/assets/icons/Previus.svg';
 
-export default function ISRDeducible() {
+export default function ISRRetenido() {
     return (
         <Layout>
             <Col sm={12} md={12}>
-                <StepperISR num='1' />
+                <StepperISR num='2' />
             </Col>
 
             <LayoutPage>
@@ -23,7 +23,7 @@ export default function ISRDeducible() {
                 <Col sm={12} md={12}>
                     <h1 className="title-card">ISR
                         <span className="span-title-card">
-                            Deducible
+                            Retenido
                         </span>
                     </h1>
                 </Col>
@@ -34,17 +34,19 @@ export default function ISRDeducible() {
                             <Col sm={12} md={5}>
                                 <div className="div-image-card">
                                     <div className="div-container-lottie-card">
-                                        <LottieISRDeducible id="lottieISRDecucilbe" className="lottie-pages" alt="ISR Deducible" />
+                                        <LottieISRRetenido id="lottieISRRetenido" className="lottie-pages" alt="ISR Retenido" />
                                     </div>
                                 </div>
                             </Col>
                             <Col sm={12} md={7}>
                                 <div className="div-container-text-card">
-                                    <p className="p-text-card">El ISR deducible se dará de las deducciones del mes,
-                                                                estas son todo gasto efectivamente pagado relacionado con tu actividad, estos se deberan considerar sin IVA o alguna retención.</p>
+                                    <p className="p-text-card">El ISR retenido es aquel que se da cuando una persona fisica
+factura servicios profesionales a una persona moral, este monto
+lo podras encontrar desglozado en tus facturas efectivamente
+cobradas en el mes.</p>
                                     
                                     <div className="div-container-input-card">
-                                        <InputMoney nombre="ISR Deducible" idInput=""Input-isrDeducible />
+                                        <InputMoney nombre="ISR Retenido" idInput=""Input-isrRetenido />
                                     </div>
                                     <div className="div-container-buttons-card">
                                         <Button className="btn-pages-np">
