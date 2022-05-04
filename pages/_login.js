924070fd20e013516ff940e411d9c3c9783a75a7
Aldahir Mayorga Garcia <samg.25sep@gmail.com>
2022-05-03 23:09:24 -0500

    Se agregan services para login y register

--- pages/_login.js
+++ pages/_login.js
@@ -4,18 +4,52 @@ import Layout from '../components/utilities/Layout';
 import TextField from '@mui/material/TextField';
 import React from "react";
 
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+// Aqui se importa el servicio
+import {loginUser} from '../services/users/index';
+
+
+const schemaLogin = yup.object({
+    email: yup.string().email('El email no es valido').required('El campo es requerido'),
+    password: yup.string().min(8, 'La longitud min es de 8 caracteres').required('El campo es requerido')
+})
+
 function Login() {
     const router = useRouter();
+
+    const { register, handleSubmit, formState:{ errors } } = useForm({
+        resolver: yupResolver(schemaLogin)
+      });
+
+     const onSubmitLogin = async (data)=>{
+         console.log('Enviando data...');
+        console.log(data);
+        const email = data.email;
+        const password = data.password;
+        //Aqui se maneja la promesa
+        const response = await loginUser(email, password);
+        const dataJson = await response.json();
+
+        console.log('Data response:',response);
+        console.log('Data dataJson:',dataJson);
+
+     }
+     console.log(errors);
+    
+
     return (
         <Layout>
         <section className="register-page-container">
-            <form className="register-page-section-form">
+            <form onSubmit ={handleSubmit(onSubmitLogin)} className="register-page-section-form">
                 <h2 className='title-card text-center'>Iniciar <span className='yellow-text'>Sesión</span></h2>
                 <p className='p-text-card text-center'>
                    Nunca compartiremos tus datos con terceros
                 </p>
 
-                <TextField
+                <TextField {...register("email")}
                     id="outlined-full-width"
                     label="Correo electronico"
                     placeholder="Correo electronico"
@@ -28,7 +62,8 @@ function Login() {
                     variant="outlined"
                     className='register-input'
                 />
-                <TextField
+                <p className = "text-danger">{errors.email?.message}</p>
+                <TextField {...register("password")}
                     id="outlined-full-width"
                     label="Contraseña"
                     placeholder="Ingrese Contraseña"
@@ -41,7 +76,8 @@ function Login() {
                     variant="outlined"
                     className='register-input'
                 />
-                <Button className="btn-contapp-navbar" onClick={()=> router.push('/dashboard/dashboardHome')}>
+                <p className = "text-danger">{errors.password?.message}</p>
+                <Button type = 'submit' className="btn-contapp-navbar" /* onClick={()=> router.push('/dashboard/dashboardHome')} */>
                     <span className="text-btn-landing">Iniciar Sesión</span>
                 </Button>
             </form>
--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,8 @@
 import react from 'react';
 import {useRouter} from 'next/router';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 import LayoutPages from '../components/utilities/layout-page/LayoutPages';
 import Layout from '../components/utilities/Layout';
 import TextField from '@mui/material/TextField';
@@ -7,6 +10,12 @@ import { Box } from '@mui/material';
 import { fontFamily } from '@mui/system';
 import { Button } from 'react-bootstrap';
 import LayoutPage from '../components/utilities/layout-page/LayoutPages';
+
+// Aqui se importa el servicio
+import {createAccount} from '../services/users/index';
+
+
+
 const styles = theme => ({
     textField: {
         width: '329px',
@@ -22,21 +31,54 @@ const styles = theme => ({
     }
 });
 
+const schemaRegister = yup.object({
+    name: yup.string().min(3, 'La longitud min es de 3 caracteres').required('El campo es requerido'),
+    surName: yup.string().min(3, 'La longitud min es de 3 caracteres').required('El campo es requerido'),
+    rfc: yup.string().min(13, 'La longitud min es de 13 caracteres').max(13,'la longitud max es de 13 caracteres').required('El campo es requerido'),
+    email: yup.string().email('El email no es valido').required('El campo es requerido'),
+    password: yup.string().min(8, 'La longitud min es de 8 caracteres').required('El campo es requerido'),
+    passwordConfirm: yup
+        .string()
+        .min(8, 'La longitud min es de 8 caracteres')
+        .required('El campo es requerido')
+        .oneOf([yup.ref('password')], 'La contraseña no coincide')
+  })
+
+
 function Register() {
     const router = useRouter();
+
+    const { register, handleSubmit, formState:{ errors } } = useForm({
+        resolver: yupResolver(schemaRegister)
+      });
+
+     const onSubmitRegister = async (data)=>{
+         console.log('Enviando data...');
+        console.log(data);
+
+        delete data.passwordConfirm //se elimina la propiedad passwordConfirm del objeto data
+        //Aqui se maneja la promesa
+        const response = await createAccount(data);
+        const dataJson = await response.json();
+
+        console.log('Data response:',response);
+        console.log('Data dataJson:',dataJson);
+
+     }
+     console.log(errors);
     return (
         <Layout>
             <section className="register-page-container">
-                <form className="register-page-section-form">
+                <form className="register-page-section-form" onSubmit={handleSubmit(onSubmitRegister)}>
                     <h2 className='title-card text-center'>Crear una <span className='yellow-text'>cuenta</span></h2>
                     <p className='p-text-card text-center'>
                         Persona fisica con actividad profesional y empresarial
                     </p>
-                    <TextField
+                    <TextField {...register("name")}
                         id="outlined-full-width"
-                        label="Nombre Completo"
+                        label="Nombre"
                         className="register-input"
-                        placeholder="Nombre completo"
+                        placeholder="Nombre"
                         fullWidth
                         margin="normal"
                         InputLabelProps={{
@@ -45,7 +87,22 @@ function Register() {
                         }}
                         variant="outlined"
                     />
-                    <TextField
+                    <p className = "text-danger">{errors.name?.message}</p>
+                     <TextField {...register("surName")}
+                        id="outlined-full-width"
+                        label="Apellido"
+                        className="register-input"
+                        placeholder="Apellido Paterno"
+                        fullWidth
+                        margin="normal"
+                        InputLabelProps={{
+                            style: { color: '#EBA53D' },
+                            shrink: true
+                        }}
+                        variant="outlined"
+                    />
+                    <p className = "text-danger">{errors.surName?.message}</p>
+                    <TextField {...register("rfc")}
                         id="outlined-full-width"
                         label="RFC"
                         placeholder="RFC"
@@ -58,11 +115,13 @@ function Register() {
                         variant="outlined"
                         className='register-input'
                     />
+                    <p className = "text-danger">{errors.rfc?.message}</p>
+
                     <h2 className='title-card text-center'>
                         Datos acceso
                     </h2>
 
-                    <TextField
+                    <TextField {...register("email")}
                         id="outlined-full-width"
                         label="Correo electronico"
                         placeholder="Correo electronico"
@@ -75,7 +134,8 @@ function Register() {
                         variant="outlined"
                         className='register-input'
                     />
-                    <TextField
+                    <p className = "text-danger">{errors.email?.message}</p>
+                    <TextField {...register("password")}
                         id="outlined-full-width"
                         label="Contraseña"
                         placeholder="Contraseña"
@@ -88,7 +148,8 @@ function Register() {
                         variant="outlined"
                         className='register-input'
                     />
-                    <TextField
+                    <p className = "text-danger">{errors.password?.message}</p>
+                    <TextField {...register("passwordConfirm")}
                         id="outlined-full-width"
                         label="Confirmar contraseña"
                         placeholder="Confirmar contraseña"
@@ -100,8 +161,8 @@ function Register() {
                         }}
                         variant="outlined"
                         className='register-input'
-                    />
-                    <Button className="Button-Register" onClick={()=> router.push('/')}>
+                    /><p className = "text-danger">{errors.passwordConfirm?.message}</p>
+                    <Button type ="submit" className="Button-Register" >
                         <span className="text-btn-landing">Crear cuenta</span>
                     </Button>
                 </form>
